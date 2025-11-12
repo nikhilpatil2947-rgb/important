3f9f75599ea7573c0fa176f6b7ea4c11587ddb27
nikhilpatil2947-rgb <nikhilpatil2947@gmail.com>
2025-11-12 12:08:12 +0530

    Add files via upload

---
+++ Custom_Helper/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ var today =DateHelper.formateDate(DateTime.Now);
+Console.WriteLine(today);
+
+var curDate =new Dateformat();
+var res  =  curDate.Date(DateTime.Now)  ;
+Console.WriteLine(res);
+
+public class DateHelper
+{
+    public static string formateDate(DateTime date,string formate = "dd-MM-yyyy")
+    {
+        return date.ToString(formate);
+    }
+
+}
+
+
+
+
+
+public class Dateformat
+{
+    public string Date(DateTime date, string formateDate = "yyyy-MM-dd")
+    {
+        return date.ToString(formateDate);
+    }
+}
